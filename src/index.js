018960905302d00d4cda5108eec27fedf06ce212
Sultan Karybaev <sultankarybaev@MacBook-Air-Sultan.local>
2018-11-29 22:09:45 +0600

    Kinoogon videoIDs

--- src/index.js
+++ src/index.js
@@ -1,145 +1,152 @@
-import http from 'http';
-import express from 'express';
-import bodyParser from 'body-parser';
-import passport from 'passport';
-import path from 'path'
-import logger from 'morgan'
-import socket from 'socket.io';
-import Message from './model/message';
-import Channel from './model/channel';
+  import http from 'http';
+  import express from 'express';
+  import bodyParser from 'body-parser';
+  import passport from 'passport';
+  import path from 'path'
+  import logger from 'morgan'
+  import socket from 'socket.io';
+  import Message from './model/message';
+  import Channel from './model/channel';
 
-const LocalStrategy  = require('passport-local').Strategy;
+  const LocalStrategy  = require('passport-local').Strategy;
 
-import config from './config';
-import routes from './routes';
+  import config from './config';
+  import routes from './routes';
 
-const apn = require('apn');
+  const apn = require('apn');
 
-console.log("hello")
-console.log(__dirname)
+  console.log("hello")
 
-let options = {
-    token: {
-        key: __dirname + "/AuthKey_44FX93V4UB.p8",
-        keyId: "44FX93V4UB",
-        teamId: "78TPJ6Q7L6"
-    },
-    production: false
-};
-let apnProvider = new apn.Provider(options);
+  let options = {
+      token: {
+          key: __dirname + "/AuthKey_44FX93V4UB.p8",
+          keyId: "44FX93V4UB",
+          teamId: "78TPJ6Q7L6"
+      },
+      production: false
+  };
+  let apnProvider = new apn.Provider(options);
 
 
-let app = express();
-app.server = http.createServer(app);
-let io = socket(app.server);
+  let app = express();
+  app.server = http.createServer(app);
+  let io = socket(app.server);
 
-//middleware
-//parse application/json
-app.use(logger('dev'))
-app.use(express.static(path.join(__dirname)));
-app.use(bodyParser.json({limit: config.bodyLimit}));
+  //middleware
+  //parse application/json
+  app.use(logger('dev'))
+  app.use(express.static(path.join(__dirname)));
+  app.use(bodyParser.json({limit: config.bodyLimit}));
 
-//passport config
-app.use(passport.initialize());
+  //passport config
+  app.use(passport.initialize());
 
-let Account = require('./model/account');
-passport.use(new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password'
-},
-  Account.authenticate()
-));
-passport.serializeUser(Account.serializeUser());
-passport.deserializeUser(Account.deserializeUser());
+  let Account = require('./model/account');
+  passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+  },
+    Account.authenticate()
+  ));
+  passport.serializeUser(Account.serializeUser());
+  passport.deserializeUser(Account.deserializeUser());
 
-//api routes v1
-app.use('/v1', routes);
+  //api routes v1
+  app.use('/v1', routes);
 
-// Base URL test endpoint to see if API is running
-app.get('/', (req, res) => {
-  res.json({ message: 'Chat API is ALIVE! Sultan Karybaev vseh udelal' })
-});
-
-app.get('/sultan', (req, res) => {
-    res.send('Hello Almaty')
-});
-
-/*||||||||||||||||SOCKET|||||||||||||||||||||||*/
-//Listen for connection
-var typingUsers = {};
-
-io.on('connection', function(client) {
-  console.log('a user connected');
-  //Listens for a new chat message
-  client.on('newChannel', function(name, description) {
-    //Create channel
-    let newChannel = new Channel({
-    name: name,
-    description: description,
+  // Base URL test endpoint to see if API is running
+  app.get('/', (req, res) => {
+    res.json({ message: 'Chat API is ALIVE! Sultan Karybaev vseh udelal' })
   });
-    //Save it to database
-    newChannel.save(function(err, channel){
-      //Send message to those connected in the room
-      console.log('new channel created');
-      io.emit("channelCreated", channel.name, channel.description, channel.id);
+
+  app.get('/sultan', (req, res) => {
+      res.send('Hello Almaty')
+  });
+
+  app.get('/youtube_kinoogon_videoids', (req, res) => {
+    res.json({
+        videoidArray: ["WoVOP8upc58", "GZQIntBz2eI", "0cljQstOKR4", "pMonTywrze8", "PzW30e8MDEA", "SBGbWEPY4tY", "nmXJOLXcIjM",
+            "qz_l1eBa_EM", "k97xoEZnq7Y", "n3JfEjz1UZs"
+        ]
+    })
+  })
+
+  /*||||||||||||||||SOCKET|||||||||||||||||||||||*/
+  //Listen for connection
+  var typingUsers = {};
+
+  io.on('connection', function(client) {
+    console.log('a user connected');
+    //Listens for a new chat message
+    client.on('newChannel', function(name, description) {
+      //Create channel
+      let newChannel = new Channel({
+      name: name,
+      description: description,
+    });
+      //Save it to database
+      newChannel.save(function(err, channel){
+        //Send message to those connected in the room
+        console.log('new channel created');
+        io.emit("channelCreated", channel.name, channel.description, channel.id);
+      });
+    });
+
+    //Listens for user typing.
+    client.on("startType", function(userName, channelId){
+      console.log("User " + userName + " is writing a message...");
+      typingUsers[userName] = channelId;
+      io.emit("userTypingUpdate", typingUsers, channelId);
+    });
+
+    client.on("stopType", function(userName){
+      console.log("User " + userName + " has stopped writing a message...");
+      delete typingUsers[userName];
+      io.emit("userTypingUpdate", typingUsers);
+    });
+
+    //Listens for a new chat message
+    client.on('newMessage', function(messageBody, userId, channelId, userName, userAvatar, userAvatarColor) {
+      //Create message
+
+      console.log(messageBody);
+
+      let newMessage = new Message({
+      messageBody: messageBody,
+      userId: userId,
+      channelId: channelId,
+      userName: userName,
+      userAvatar: userAvatar,
+      userAvatarColor: userAvatarColor
+    });
+      //Save it to database
+      newMessage.save(function(err, msg){
+        //Send message to those connected in the room
+        console.log('new message sent');
+
+        io.emit("messageCreated",  msg.messageBody, msg.userId, msg.channelId, msg.userName, msg.userAvatar, msg.userAvatarColor, msg.id, msg.timeStamp);
+          let deviceToken = "1cc19290ef02d0238d49e3cf34928050f0870627a1a5a8297c8ffd20e80e217d";
+          let notification = new apn.Notification();
+          notification.expiry = Math.floor(Date.now() / 1000) + 24 * 3600; // will expire in 24 hours from now
+          notification.badge = 2;
+          notification.sound = "ping.aiff";
+          notification.alert = msg.messageBody;
+          notification.payload = {'messageFrom': 'Solarian Programmer'};
+          notification.topic = "com.sultankarybaev.smack";
+          console.log(notification)
+          apnProvider.send(notification, deviceToken).then( result => {
+              console.log(result);
+          });
+          apnProvider.shutdown();
+      });
     });
   });
+  /*||||||||||||||||||||END SOCKETS||||||||||||||||||*/
 
-  //Listens for user typing.
-  client.on("startType", function(userName, channelId){
-    console.log("User " + userName + " is writing a message...");
-    typingUsers[userName] = channelId;
-    io.emit("userTypingUpdate", typingUsers, channelId);
-  });
+  app.server.listen(config.port);
+  console.log(`Started on port ${app.server.address().port}`);
 
-  client.on("stopType", function(userName){
-    console.log("User " + userName + " has stopped writing a message...");
-    delete typingUsers[userName];
-    io.emit("userTypingUpdate", typingUsers);
-  });
-
-  //Listens for a new chat message
-  client.on('newMessage', function(messageBody, userId, channelId, userName, userAvatar, userAvatarColor) {
-    //Create message
-
-    console.log(messageBody);
-
-    let newMessage = new Message({
-    messageBody: messageBody,
-    userId: userId,
-    channelId: channelId,
-    userName: userName,
-    userAvatar: userAvatar,
-    userAvatarColor: userAvatarColor
-  });
-    //Save it to database
-    newMessage.save(function(err, msg){
-      //Send message to those connected in the room
-      console.log('new message sent');
-
-      io.emit("messageCreated",  msg.messageBody, msg.userId, msg.channelId, msg.userName, msg.userAvatar, msg.userAvatarColor, msg.id, msg.timeStamp);
-        let deviceToken = "1cc19290ef02d0238d49e3cf34928050f0870627a1a5a8297c8ffd20e80e217d";
-        let notification = new apn.Notification();
-        notification.expiry = Math.floor(Date.now() / 1000) + 24 * 3600; // will expire in 24 hours from now
-        notification.badge = 2;
-        notification.sound = "ping.aiff";
-        notification.alert = msg.messageBody;
-        notification.payload = {'messageFrom': 'Solarian Programmer'};
-        notification.topic = "com.sultankarybaev.smack";
-        console.log(notification)
-        apnProvider.send(notification, deviceToken).then( result => {
-            console.log(result);
-        });
-        apnProvider.shutdown();
-    });
-  });
-});
-/*||||||||||||||||||||END SOCKETS||||||||||||||||||*/
-
-app.server.listen(config.port);
-console.log(`Started on port ${app.server.address().port}`);
-
-module.exports = {
-  app,
-  io
-}
+  module.exports = {
+    app,
+    io
+  }
